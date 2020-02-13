9d2dd13b058b40d04e97e18b6d655eb83b5ff016
Paul von Hagen <pvonhagen26@gmail.com>
2020-02-13 03:30:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,3 +7,7 @@ def batch_badge_creator(attendees)
   attendees.each {|badge| batch_badge_array << "Hello, my name is #{badge}."}
   batch_badge_array
 end
+
+def assign_rooms(attendees)
+  room_assignments = []
+  attendees.each_with_index {|room| room_assignments << "Hello, #{attendees}! You'll be assigned to room #{room + 1}!"}
