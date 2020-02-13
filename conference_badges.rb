5cd5b3761ddf54704c41ec08be4e9e94d31218a5
Paul von Hagen <pvonhagen26@gmail.com>
2020-02-13 03:25:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,6 +4,6 @@ end
 
 def batch_badge_creator(attendees)
   batch_badge_array = []
-  attendees.each {|badge| batch_badge_array.push "Hello, my name is #{badge}"}
+  attendees.each {|badge| batch_badge_array.push "Hello, my name is #{badge}."}
   batch_badge_array
 end
